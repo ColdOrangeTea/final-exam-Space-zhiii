8fee6009294ed02382074678504cd7604ce3ba8f
DESKTOP-523DDLO\Li-Qiao <a110082029@mail.shu.edu.tw>
2023-01-09 03:49:14 +0800

    Feat: BeginningStory的camMove程式製作 by小封

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -6,22 +6,35 @@ public class CameraMove : MonoBehaviour
 {
     [SerializeField]
     private Camera cam;
-    private Vector3 dragOrigin;
+    public Vector3[] v = {new Vector3(0, 0, -10), new Vector3(0, -12.3f, -10), new Vector3(-1.96f, 0, -10), new Vector3(-1.96f, -12.3f, -10), new Vector3(-13, 0, -10), new Vector3(-13, -12.3f, -10) };
+    int j = 0;
+
+    void Start()
+    {
+        // 協程開始
+        StartCoroutine(CamMove());
+    }
 
     private void Update()
     {
-        PanCamera();
+        // 紀錄現在位置
+        Vector3 camTarget = cam.transform.position;
+
+        // 緩慢移動至指定的位置
+        cam.transform.position = Vector3.Lerp(camTarget, v[j], 10 * Time.deltaTime);
     }
-    private void PanCamera()
+
+    // 協程
+    IEnumerator CamMove()
     {
-        if (Input.GetMouseButtonDown(0))
-           dragOrigin = cam.ScreenToWorldPoint(Input.mousePosition);
-        
-        if (Input.GetMouseButton(0))
+        // for迴圈
+        for (int i = 0; i < 5; i++)
         {
-            Vector3 difference = dragOrigin - cam.ScreenToWorldPoint(Input.mousePosition);
-            print("origin" + dragOrigin + "newPosition" + cam.ScreenToWorldPoint(Input.mousePosition) + "=difference" +difference );
-            cam.transform.position += difference;
+            // 停兩秒
+            yield return new WaitForSeconds(2);
+
+            // 照順序更改指定位置
+            j++;
         }
     }
 }
